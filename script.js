14169b8763816ce8de48a6595ceb1adf96c7c3e6
Thomas Lo <thomasjinlo@gmail.com>
2016-03-17 10:06:00 -0700

    AJAXgit add -A

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 
-ourAjax = {
+var ourAjax = {
 
 
-  ajax: function( ourOptions ) {
+  ajax: function( options ) {
+    var xhr = new XMLHttpRequest();
 
+    xhr.open(options.type, options.url, options.async);
+
+
+    xhr.onload = function ( event ) {
+      if ( xhr.readyState === 4 ) {
+
+        if ( xhr.status >= 200 && xhr.status <= 300) {
+          options.success(xhr.responseText);
+        } else {
+          options.error(xhr, xhr.status, xhr.statusText)
+        }
+        options.complete(xhr, xhr.status)
+      }
+    };
+
+    
+
+    xhr.send();
   }
 
-},
+};
 
 
-options = {
+var blankOptions = {
   complete: function(){},
   data: {},
   error: function() {}, 
@@ -18,28 +37,36 @@ options = {
   success: function() {},
   url: "",
   async: true,
-}
+};
 
 
-ourOptions = options;
+var options = blankOptions;
 
 var domain = "http://jsonplaceholder.typicode.com/";
 var path = "posts/4";
 var type = "GET";
 var success = function(data) {
+  console.log("it gets here");
   console.log(data);
 };
+var complete = function(xhr, status) {
+  alert("The request is complete with status: " + status)
+};
+var error = function(xhr, status, errorThrown) {
+  alert( "Sorry, there was a problem!" );
+  console.log( "Error: " + errorThrown );
+  console.log( "Status: " + status );
+  console.dir( xhr );
+}
 
-ourOptions.url = domain + path;
-ourOptions.type = type;
-ourOptions.success = success;
+options.url = domain + path;
+options.type = type;
+options.success = success;
+options.complete = complete;
+options.error = error;
 
-$.ajax( ourOptions );   // jquery ajax way
+var $ = ourAjax;
 
-
-// $ = ourAjax;
-// ourAjax.ajax( ourOptions );
-
-
+$.ajax( options );   // jquery ajax way
 
 
